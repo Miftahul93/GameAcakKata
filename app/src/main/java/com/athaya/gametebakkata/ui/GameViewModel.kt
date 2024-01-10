cb05fa78bd0a6da95661b0e2ac1e2d71e505fc67
Miftahul93 <miftahululum693@gmail.com>
2024-01-10 21:56:57 +0700

    Memperbarui kata acak

--- app/src/main/java/com/athaya/gametebakkata/ui/GameViewModel.kt
+++ app/src/main/java/com/athaya/gametebakkata/ui/GameViewModel.kt
@@ -4,6 +4,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import com.athaya.gametebakkata.data.SCORE_INCREASE
 import com.athaya.gametebakkata.data.allWords
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
@@ -59,16 +60,29 @@ class GameViewModel : ViewModel() {
 
     fun checkUserGuess() {
         if (userGuess.equals(currentWord, ignoreCase = true)) {
-
+            // User's guess is correct, increase the score
+            // and call updateGameState() to prepare the game for next round
+            val updateScore = _uiState.value.score.plus(SCORE_INCREASE)
+            updateGameState(updateScore)
         } else {
             // User's guess is wrong,show an error
             _uiState.update { currentState ->
                 currentState.copy(isGuessedWordWrong = true)
             }
-
         }
         // Reset user gues
         updateUserGuess("")
+    }
+
+    fun updateGameState(updateScore: Int) {
+        _uiState.update { currentState ->
+            currentState.copy(
+                isGuessedWordWrong = false,
+                currentScrambledWord = pickRandomWordAndShuffle(),
+                score = updateScore,
+                currentWordCount = currentState.currentWordCount.inc()
+            )
+        }
     }
 }
 
